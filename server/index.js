f86504b6fdfa032f8d2820570e44928ee3968677
Jetty2020 <kmclo@naver.com>
2021-02-12 01:00:48 +0900

    Slightly modified

--- server/index.js
+++ server/index.js
@@ -31,11 +31,6 @@ app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
 
-//use this to show the image you have in node js server to client (react js)
-//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
-app.use('/uploads', express.static('uploads'));
-
-
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
