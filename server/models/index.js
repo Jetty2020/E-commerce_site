cd6dfa50bb332ddc0ab653c997035578120650e9
song <johyesong8686@gmail.com>
2021-03-04 12:07:42 +0900

    feat: DB 관계 재 모델링

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,29 @@
 import Sequelize from "sequelize";
 
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.User = require('./user')(sequelize, Sequelize);
-db.Product = require('./product')(sequelize, Sequelize);
+db.User = require("./user")(sequelize, Sequelize);
+db.Product = require("./product")(sequelize, Sequelize);
 
-db.User.hasMany(db.Product, { foreignKey: 'producter', sourceKey: 'id'});
-db.Product.belongsTo(db.User, { foreignKey: 'producter', targetKey: 'id'});
+db.User.belongsToMany(db.Product, { as: "cartProduct", through: "cart" });
+db.Product.belongsToMany(db.User, { as: "userCart", through: "cart" });
+
+db.User.belongsToMany(db.Product, { as: "wishList", through: "wish" });
+db.Product.belongsToMany(db.User, { as: "userWish", through: "wish" });
+
+db.User.belongsToMany(db.Product, { as: "commentProduct", through: "comment" });
+db.Product.belongsToMany(db.User, { as: "userComment", through: "comment" });
 
 module.exports = db;
