2ec25de164edca0f8ad48e865cd19b9584fdf9b7
eunbin-lee <euns2bin@gmail.com>
2021-03-11 08:46:29 +0900

    Menu name changed

--- client_eb/src/components/views/WishlistPage/WishlistPage.js
+++ client_eb/src/components/views/WishlistPage/WishlistPage.js
@@ -1,59 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ProductsList from '../../utils/ProductsList';
+import { products } from '../../../_datas/productsData.json';
 
 function WishlistPage() {
-  const imageUrl =
-    'https://www.kingplastic.com/wp-content/uploads/2014/12/Charcoal-Gray-300x300.jpg';
-
-  //example wishlist
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      image: imageUrl,
-      name: `Product 1`,
-      price: `00000￦`,
-      likes: `234`,
-      reviews: `10`,
-      wishlist: true,
-    },
-    {
-      id: 2,
-      image: imageUrl,
-      name: `Product 2`,
-      price: `00000￦`,
-      likes: `234`,
-      reviews: `10`,
-      wishlist: true,
-    },
-    {
-      id: 3,
-      image: imageUrl,
-      name: `Product 3`,
-      price: `00000￦`,
-      likes: `234`,
-      reviews: `10`,
-      wishlist: true,
-    },
-    {
-      id: 4,
-      image: imageUrl,
-      name: `Product 4`,
-      price: `00000￦`,
-      likes: `234`,
-      reviews: `10`,
-      wishlist: true,
-    },
-  ]);
-
+  const WISHLIST = products.slice(20, 25);
+  // console.log(WISHLIST);
   const onRemove = (id) => {
-    setProducts(products.filter((product) => product.id !== id));
+    products.filter((product) => product.id !== id);
   };
 
   return (
     <div style={{ width: '75%', margin: '3rem auto' }}>
       <h2 style={{ fontWeight: 'bold' }}>위시리스트</h2>
 
-      <ProductsList products={products} onRemove={onRemove} />
+      <ProductsList products={WISHLIST} onRemove={() => onRemove()} />
     </div>
   );
 }
