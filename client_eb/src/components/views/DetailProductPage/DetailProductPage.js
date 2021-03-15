9eb1a90f7d4db310f16080dabe18fecc76aff869
Jetty2020 <kmclo@naver.com>
2021-03-16 00:09:19 +0900

    2021-3-16 DetailProductPage.js Delete useless part

--- client_eb/src/components/views/DetailProductPage/DetailProductPage.js
+++ client_eb/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
+import React from 'react';
 import { products } from '../../../_datas/productsData.json';
 import { Row, Col } from 'antd';
 import ProductImage from './Sections/ProductImage';
 import ProductInfo from './Sections/ProductInfo';
-import { addToCart } from '../../../_actions/user_actions';
-// import { useDispatch } from 'react-redux';
 import ProductDetails from './Sections/ProductDetails';
 
 function DetailProductPage(props) {
@@ -18,14 +15,6 @@ function DetailProductPage(props) {
       }
     }
   };
-
-  useEffect(() => {
-    product();
-  }, []);
-
-  // const addToCartHandler = (productId) => {
-  //   dispatch(addToCart(productId));
-  // };
 
   return (
     <div
@@ -42,7 +31,6 @@ function DetailProductPage(props) {
         </Col>
         <Col lg={12} xs={24} style={{ width: '40%' }}>
           <ProductInfo
-            // addToCart={addToCartHandler}
             detail={product()}
           />
         </Col>
