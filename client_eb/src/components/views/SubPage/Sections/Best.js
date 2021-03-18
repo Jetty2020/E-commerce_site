87cd79ed6e2885c2741b9e685431394823b4e426
Jetty2020 <kmclo@naver.com>
2021-03-18 23:13:28 +0900

    2021-3-18 Delete filter in subpage

--- client_eb/src/components/views/SubPage/Sections/Best.js
+++ client_eb/src/components/views/SubPage/Sections/Best.js
@@ -12,23 +12,6 @@ const Best = () => {
     <div>
       <h2 style={{ margin: '70px 5px 30px' }}>Best</h2>
 
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          margin: '20px 5px',
-        }}
-      >
-        <Select defaultValue="recommendation_order" style={{ width: '150px' }}>
-          <Option value="recommendation_order">추천순</Option>
-          <Option value="new_product_order">신상품순</Option>
-          <Option value="low_price_order">낮은가격순</Option>
-          <Option value="high_price_order">높은가격순</Option>
-          <Option value="best_likes_order">베스트하트순</Option>
-          <Option value="best_review_order">베스트리뷰순</Option>
-        </Select>
-      </div>
-
       {/* products */}
       <ProductsList products={BEST} />
     </div>
