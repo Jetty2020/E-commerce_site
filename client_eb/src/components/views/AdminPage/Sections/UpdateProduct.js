378d52b9dd952ceb1d1bd8ae9d61ab7afa985b11
eunbin-lee <euns2bin@gmail.com>
2021-03-23 21:38:08 +0900

    2021-3-23 Update UI in  AdminPage.js

--- client_eb/src/components/views/AdminPage/Sections/UpdateProduct.js
+++ client_eb/src/components/views/AdminPage/Sections/UpdateProduct.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { productDetail } from "../../../../_actions/product_actions";
-import { Formik } from "formik";
-import * as Yup from "yup";
-import { Typography, Button, Form, Input } from "antd";
-import { editProduct } from "../../../../_actions/product_actions";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { productDetail } from '../../../../_actions/product_actions';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { Typography, Button, Form, Input, Select } from 'antd';
+import { editProduct } from '../../../../_actions/product_actions';
 
 const { Title } = Typography;
+const { Option } = Select;
 
 function UpdateProduct(props) {
   const dispatch = useDispatch(); //dispatch for redux
@@ -29,7 +30,7 @@ function UpdateProduct(props) {
         alert(err);
       });
   }
-  const [formErrorMessage, setFormErrorMessage] = useState("");
+  const [formErrorMessage, setFormErrorMessage] = useState('');
 
   return (
     <>
@@ -43,11 +44,12 @@ function UpdateProduct(props) {
               rate: productDe.rate,
             }}
             validationSchema={Yup.object().shape({
-              rate: Yup.number("할인율을 숫자로 입력해 주세요")
-                .integer("할인율을 정확히 입력해 주세요"),
-              price: Yup.number("가격을 숫자로 입력해 주세요")
-                .positive("가격을 양수로 입력해 주세요")
-                .integer("가격을 정확히 입력해 주세요"),
+              rate: Yup.number('할인율을 숫자로 입력해 주세요').integer(
+                '할인율을 정확히 입력해 주세요',
+              ),
+              price: Yup.number('가격을 숫자로 입력해 주세요')
+                .positive('가격을 양수로 입력해 주세요')
+                .integer('가격을 정확히 입력해 주세요'),
             })}
             onSubmit={(values, { setSubmitting }) => {
               setTimeout(() => {
@@ -61,14 +63,14 @@ function UpdateProduct(props) {
                     if (response.payload.success) {
                       props.history.push(`/product/${productId}`);
                     } else {
-                      setFormErrorMessage("Error occurred"); //에러 메세지 세팅
+                      setFormErrorMessage('Error occurred'); //에러 메세지 세팅
                     }
                   })
                   .catch((err) => {
-                    setFormErrorMessage("Error occurred");
+                    setFormErrorMessage('Error occurred');
                     setTimeout(() => {
                       //일정 시간이 지난 후 함수 실행 setTimeout(실행시킬 함수, 시간)
-                      setFormErrorMessage("");
+                      setFormErrorMessage('');
                     }, 3000);
                   });
                 setSubmitting(false);
@@ -86,40 +88,46 @@ function UpdateProduct(props) {
                 handleSubmit,
               } = props;
               return (
-                <div style={{ maxWidth: "500px", margin: "3rem auto" }}>
-                  <div style={{ marginBottom: "2rem" }}>
+                <div style={{ maxWidth: '500px', margin: '3rem auto' }}>
+                  <div style={{ marginBottom: '2rem' }}>
                     <Title level={3}>상품 수정</Title>
                   </div>
                   <form
                     onSubmit={handleSubmit}
-                    style={{ width: "500px" }}
+                    style={{ width: '500px' }}
                     encType="multipart/form-data"
                   >
                     {/* 기등록된 이미지 */}
-                    <img
-                      src={values.mainImg}
-                      alt="mainImg"
-                      style={{ width: "400px", marginTop: "20px" }}
-                    />
-                    <Form.Item label={"상품명"} required>
+                    <div style={{ width: '420px', margin: '20px auto 30px' }}>
+                      <img src={values.mainImg} alt="mainImg" width="100%" />
+                    </div>
+                    <Form.Item label={'상품명'} required>
                       <Input
                         id="name"
                         disabled={true}
-                        placeholder="Product name"
                         value={values.name}
                         onChange={handleChange}
                         onBlur={handleBlur}
                         className={
                           errors.name && touched.name
-                            ? "text-input error"
-                            : "text-input"
+                            ? 'text-input error'
+                            : 'text-input'
                         }
                       />
                       {errors.name && touched.name && (
                         <div className="input-feedback">{errors.name}</div>
                       )}
                     </Form.Item>
-                    <Form.Item label={"판매금액(원)"} required>
+                    <Form.Item label={'카테고리'}>
+                      <Select>
+                        <Option value="all">All</Option>
+                        <Option value="best">Best</Option>
+                        <Option value="new">New</Option>
+                        <Option value="discount">Discount</Option>
+                        <Option value="recommend">Recommend</Option>
+                      </Select>
+                    </Form.Item>
+                    <Form.Item label={'판매금액(원)'} required>
                       <Input
                         id="price"
                         value={values.price}
@@ -127,15 +135,15 @@ function UpdateProduct(props) {
                         onBlur={handleBlur}
                         className={
                           errors.price && touched.price
-                            ? "text-input error"
-                            : "text-input"
+                            ? 'text-input error'
+                            : 'text-input'
                         }
                       />
                       {errors.price && touched.price && (
                         <div className="input-feedback">{errors.price}</div>
                       )}
                     </Form.Item>
-                    <Form.Item label={"할인율(%)"}>
+                    <Form.Item label={'할인율(%)'}>
                       <Input
                         id="rate"
                         value={values.rate}
@@ -143,8 +151,8 @@ function UpdateProduct(props) {
                         onBlur={handleBlur}
                         className={
                           errors.rate && touched.rate
-                            ? "text-input error"
-                            : "text-input"
+                            ? 'text-input error'
+                            : 'text-input'
                         }
                       />
                       {errors.rate && touched.rate && (
@@ -153,21 +161,21 @@ function UpdateProduct(props) {
                     </Form.Item>
                     <div
                       style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        marginTop: "30px",
+                        display: 'flex',
+                        justifyContent: 'center',
+                        marginTop: '30px',
                       }}
                     >
                       <Button
                         type="primary"
                         htmlType="submit"
-                        style={{ margin: "0 2px" }}
+                        style={{ margin: '0 2px' }}
                         disabled={isSubmitting}
                         onSubmit={handleSubmit}
                       >
                         수정
                       </Button>
-                      <Button style={{ margin: "0 2px" }}>
+                      <Button style={{ margin: '0 2px' }}>
                         <Link to="/admin">취소</Link>
                       </Button>
                     </div>
